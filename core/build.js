1a3dafbffc37258f69302d75a5ce5f84edb13c20
Jaysaurus <the.jaysaurus@gmail.com>
2017-10-02 23:44:39 +0100

    - all codepaths in core/build.js tested - build.js error handling refined

--- core/build.js
+++ core/build.js
@@ -2,15 +2,16 @@ const fs = require('fs');
 
 let appendFileCommands = (files, type, next) => {
   if (files) {
+    const dirName = `${type.toLowerCase()}s`;
     files.forEach((file) => {
       const suffix = type.match(/[A-Za-z]+$/); // e.g. dir/SubDir -> "SubDir"
       const name = file.replace(/\.js$/, ''); // e.g. file.js -> "file"
       if (nameMatchesType(name, type)) {
-        const requirement = require(`${__root}/api/${type.toLowerCase()}s/${name}`);
+        const requirement = require(`${__root}/api/${dirName}/${name}`);
         const objectName = name.replace((suffix ? suffix[0] : ''), '');
         next(requirement, objectName);
       } else {
-        process.log(`Spotted "${file}" in "/${type}". Build will not action this item.`);
+        process.log(`Spotted "${file}" in "/${dirName}". Build will not action this item.`);
       }
     });
   }
@@ -21,15 +22,16 @@ let nameMatchesType = (name, type) => name.match(`${type.replace(/^[A-Za-z]+\//,
 module.exports = {
   build: (type, $) => {
     try {
-      const dir = `${__root}/api/${type.toLowerCase()}s/`;
+      const dirName = `${type.toLowerCase()}s`;
+      const dir = `${__root}/api/${dirName}/`;
       if (!fs.existsSync(dir)) {
-        throw new Error(`directory "${type}" doesn't exist`);
+        throw new Error(`directory "${dirName}" doesn't exist`);
       } else {
         appendFileCommands(fs.readdirSync(dir), type, $);
         process.log(`The ${type.match(/[A-Za-z0-9]+$/)}s have been imported`);
       }
     } catch (e) {
-      throw new Error(`Failed to fetch ${type}: ${e.stack}`);
+      throw new Error(`Failed to build ${type}s: ${e.stack}`);
     }
     return $;
   }
